b119bc189f1244def00566f5a4b493c22ff2f255
trentondenton <trentondenton91@gmail.com>
2022-02-04 12:44:18 -0600

    Update slideshow.js
    changed links to "all who have a link can view"

--- src/components/pages/slideshow.js
+++ src/components/pages/slideshow.js
@@ -8,7 +8,7 @@ import ReactGoogleSlides from "react-google-slides";
      super(props);
 
      this.state= {
-       link: "https://docs.google.com/presentation/d/1u4fSc-b2UcI1Y8rdHWuw4GlCOGqm4WrNGrKuTwpHIaU/edit#slide=id.p",
+       link: "https://docs.google.com/presentation/d/1u4fSc-b2UcI1Y8rdHWuw4GlCOGqm4WrNGrKuTwpHIaU/edit?usp=sharing",
        heading: "Course Projects",
        description: "Please Select One"
      }
@@ -31,7 +31,7 @@ import ReactGoogleSlides from "react-google-slides";
         <div className="google-slide-selectors">
           <button id="toggle" 
             onClick={() => this.updateLink(
-              "https://docs.google.com/presentation/d/1vRB2DnzmDd9T34s_I6l0X2w85Lt7-V4GlDxBEjSIzZg/edit#slide=id.p1",
+              "https://docs.google.com/presentation/d/1shxHeDm9KSVpD8xIHKuM12l6yfHzuopnUREYdGtCx1E/edit?usp=sharing",
               "CEIS 101",
               "Internet of Things")}>
             CEIS 101 
@@ -39,7 +39,7 @@ import ReactGoogleSlides from "react-google-slides";
 
           <button id="toggle" 
             onClick={() => this.updateLink(
-              "https://docs.google.com/presentation/d/1vRB2DnzmDd9T34s_I6l0X2w85Lt7-V4GlDxBEjSIzZg/edit#slide=id.p1",
+              "https://docs.google.com/presentation/d/1vRB2DnzmDd9T34s_I6l0X2w85Lt7-V4GlDxBEjSIzZg/edit?usp=sharing",
               "CEIS 106",
               "Operating Systems")}> 
             CEIS 106
@@ -47,14 +47,14 @@ import ReactGoogleSlides from "react-google-slides";
 
           <button id="toggle" 
             onClick={() => this.updateLink(
-              "https://docs.google.com/presentation/d/1fGtjlgDR-y3PHFhpmlE_GQIUiBUDi9STHpXt2uldQdg/edit#slide=id.p1",
+              "https://docs.google.com/presentation/d/1fGtjlgDR-y3PHFhpmlE_GQIUiBUDi9STHpXt2uldQdg/edit?usp=sharing",
               "CEIS 110", "Intro to Programming")}>
             CEIS 110
            </button>
 
           <button id="toggle" 
             onClick={() => this.updateLink(
-              "https://docs.google.com/presentation/d/1aFcYNYkOilV2xvgeFnIyPHPS9M_6CgUb1NUiTNWYuqs/edit#slide=id.p1",
+              "https://docs.google.com/presentation/d/1aFcYNYkOilV2xvgeFnIyPHPS9M_6CgUb1NUiTNWYuqs/edit?usp=sharing",
               "CEIS 114",
               "Digital Devices")}>
             CEIS 114
@@ -62,7 +62,7 @@ import ReactGoogleSlides from "react-google-slides";
 
           <button id="toggle"
             onClick={() => this.updateLink(
-              "https://docs.google.com/presentation/d/1usUOtFocMw0L6fAIf7XpvbLEU1JG5IkZ_2vCC2BMH6k/edit#slide=id.p1",
+              "https://docs.google.com/presentation/d/1usUOtFocMw0L6fAIf7XpvbLEU1JG5IkZ_2vCC2BMH6k/edit?usp=sharing",
               "CIS 170C",
               "Fundamentals of Programming")}>
             CIS 170C
@@ -70,21 +70,21 @@ import ReactGoogleSlides from "react-google-slides";
 
           <button id="toggle" 
             onClick={() => this.updateLink(
-              "https://docs.google.com/presentation/d/1xpi1NQBWqbx-TBiiJg1IaW4VhcImZQ5Fd6ZRu7ZK7HA/edit#slide=id.p1",
+              "https://docs.google.com/presentation/d/1xpi1NQBWqbx-TBiiJg1IaW4VhcImZQ5Fd6ZRu7ZK7HA/edit?usp=sharing",
               "NETW 191",
               "Intro to IT & Networking")}>
               NETW 191
           </button>
 
           <button id="toggle" onClick={() => this.updateLink(
-            "https://docs.google.com/presentation/d/1Tmie2ISje5OCSbakt5loOt_PkUYc8AtX6_Di2-kJPt8/edit#slide=id.p1",
+            "https://docs.google.com/presentation/d/1Tmie2ISje5OCSbakt5loOt_PkUYc8AtX6_Di2-kJPt8/edit?usp=sharing",
             "NETW 211",
             "Cloud Computing")}>
             NETW 211
           </button>
 
           <button id="toggle" onClick={() => this.updateLink(
-            "https://docs.google.com/presentation/d/11V1pyLzDAaRDZkiY3HCt0JVmN7nufggpE9f1CF9L0iE/edit#slide=id.p2",
+            "https://docs.google.com/presentation/d/11V1pyLzDAaRDZkiY3HCt0JVmN7nufggpE9f1CF9L0iE/edit?usp=sharing",
             "SEC 285",
             "Fundamentals of Information Security")}>
             SEC 285
