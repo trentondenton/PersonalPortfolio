291a092ad4c3bf6374ac7fbe03ca95eae3de94c5
trentondenton <trentondenton91@gmail.com>
2022-03-06 15:53:23 -0600

    Update slideshow.js

--- src/components/pages/slideshow.js
+++ src/components/pages/slideshow.js
@@ -3,56 +3,56 @@ import ReactGoogleSlides from "react-google-slides";
 
 
 
- class Slideshow extends Component {
-   constructor(props) {
-     super(props);
+class Slideshow extends Component {
+  constructor(props) {
+    super(props);
 
-     this.state= {
-       link: "https://docs.google.com/presentation/d/1u4fSc-b2UcI1Y8rdHWuw4GlCOGqm4WrNGrKuTwpHIaU/edit?usp=sharing",
-       heading: "Course Projects",
-       description: "Please Select One"
-     }
-     
-     this.updateLink = this.updateLink.bind(this);
-   }
+    this.state = {
+      link: "https://docs.google.com/presentation/d/1u4fSc-b2UcI1Y8rdHWuw4GlCOGqm4WrNGrKuTwpHIaU/edit?usp=sharing",
+      heading: "Course Projects",
+      description: "Please Select One"
+    }
 
-   updateLink = (glink, header, descript) => {
-     this.setState({ 
-       link: glink,
-       heading: header,
-       description: descript
-      });
-   }
-   
-   render() {
+    this.updateLink = this.updateLink.bind(this);
+  }
+
+  updateLink = (glink, header, descript) => {
+    this.setState({
+      link: glink,
+      heading: header,
+      description: descript
+    });
+  }
+
+  render() {
 
     return (
       <div className="google-slide-page">
         <div className="google-slide-selectors">
-          <button id="toggle" 
+          <button id="toggle"
             onClick={() => this.updateLink(
               "https://docs.google.com/presentation/d/1shxHeDm9KSVpD8xIHKuM12l6yfHzuopnUREYdGtCx1E/edit?usp=sharing",
               "CEIS 101",
               "Internet of Things")}>
-            CEIS 101 
+            CEIS 101
           </button>
 
-          <button id="toggle" 
+          <button id="toggle"
             onClick={() => this.updateLink(
               "https://docs.google.com/presentation/d/1vRB2DnzmDd9T34s_I6l0X2w85Lt7-V4GlDxBEjSIzZg/edit?usp=sharing",
               "CEIS 106",
-              "Operating Systems")}> 
+              "Operating Systems")}>
             CEIS 106
           </button>
 
-          <button id="toggle" 
+          <button id="toggle"
             onClick={() => this.updateLink(
               "https://docs.google.com/presentation/d/1fGtjlgDR-y3PHFhpmlE_GQIUiBUDi9STHpXt2uldQdg/edit?usp=sharing",
               "CEIS 110", "Intro to Programming")}>
             CEIS 110
-           </button>
+          </button>
 
-          <button id="toggle" 
+          <button id="toggle"
             onClick={() => this.updateLink(
               "https://docs.google.com/presentation/d/1aFcYNYkOilV2xvgeFnIyPHPS9M_6CgUb1NUiTNWYuqs/edit?usp=sharing",
               "CEIS 114",
@@ -68,12 +68,12 @@ import ReactGoogleSlides from "react-google-slides";
             CIS 170C
           </button>
 
-          <button id="toggle" 
+          <button id="toggle"
             onClick={() => this.updateLink(
               "https://docs.google.com/presentation/d/1xpi1NQBWqbx-TBiiJg1IaW4VhcImZQ5Fd6ZRu7ZK7HA/edit?usp=sharing",
               "NETW 191",
               "Intro to IT & Networking")}>
-              NETW 191
+            NETW 191
           </button>
 
           <button id="toggle" onClick={() => this.updateLink(
@@ -89,11 +89,18 @@ import ReactGoogleSlides from "react-google-slides";
             "Fundamentals of Information Security")}>
             SEC 285
           </button>
-          
+          https://docs.google.com/presentation/d/14I45OShHTkDW9gTnl1o9RPdUvW7_ilJJjXgXwhKgvr4/edit?usp=sharing
+
+          <button id="toggle" onClick={() => this.updateLink(
+            "https://docs.google.com/presentation/d/14I45OShHTkDW9gTnl1o9RPdUvW7_ilJJjXgXwhKgvr4/edit?usp=sharing",
+            "CEIS 239",
+            "Database Systems")}>
+            CEIS 239
+          </button>
         </div>
 
         <div className="course-slide">
-          <h1>{this.state.heading}</h1> 
+          <h1>{this.state.heading}</h1>
           <p>{this.state.description}</p>
           <ReactGoogleSlides
             width={800}
@@ -104,7 +111,7 @@ import ReactGoogleSlides from "react-google-slides";
             showControls
             loop
           />
-          </div>
+        </div>
       </div>
     )
   }
