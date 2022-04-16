14b262869af7296212b83e97cdd9ef341f979b36
Trenton <trentondenton91@gmail.com>
2022-04-16 13:41:00 -0500

    Update login.js

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -63,29 +63,29 @@ export default class Login extends Component {
         <div>{this.state.errorText}</div>
 
         <form onSubmit={this.handleSubmit} className="auth-form-wrapper">
-        <div className="form-group">
-          <FontAwesomeIcon icon="envelope" />
-          <input
-            type="email"
-            name="email"
-            placeholder="Your email"
-            value={this.state.email}
-            onChange={this.handleChange}
-          />
-        </div>
+          <div className="form-group">
+            <FontAwesomeIcon icon="envelope" />
+            <input
+              type="email"
+              name="email"
+              placeholder="Your email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </div>
 
-        <div className="form-group">
-          <FontAwesomeIcon icon="lock" />
-          <input
-            type="password"
-            name="password"
-            placeholder="Your password"
-            value={this.state.password}
-            onChange={this.handleChange}
-          />
-        </div>
+          <div className="form-group">
+            <FontAwesomeIcon icon="lock" />
+            <input
+              type="password"
+              name="password"
+              placeholder="Your password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+          </div>
 
-        <button className="btn" type="submit">Login</button>
+          <button className="btn" type="submit">Login</button>
         </form>
       </div>
     );
