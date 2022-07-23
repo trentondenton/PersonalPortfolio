4a89197722d69c8cab2ba53c04deedd6232b0140
Ace Denton <trentondenton91@gmail.com>
2022-07-23 14:18:46 -0500

    updated about me

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -17,17 +17,14 @@ export default function () {
       <div className="right-column">
         <h1>Hello! Thank you for visiting.</h1>
         <p>
-          My name is Trenton. I'm an entry level developer. I am currently enrolled
-          at Devry University. I will complete my Associates in Information Technology and Networking in April of 2022.
-          I will continue working towards my Bachelors in Computer Information Systems with a focus in Software Development.
+          My name is Trenton. I'm an entry level developer. I completed my Associate's in Information Technology in June of 2022.
+          I will continue working towards my Bachelors in Computer Information Systems with a focus in Software Development from DeVry University.
         </p>
         <p>
-          I worked in nursing for 12years and after the intital COVID-19 pandemic began I decided to persue a
-          career in a field I have always wanted to work in. I have completed a coding bootcamp form Bottega University
-          through the Kansas Rural & Remote Program and received a Full Stack Development Certificate. I have also
-          completed the Google Data Analyst Course through Coursera. I will be taking the Python Institute PCEP Test in
-          April of 2022. The blog page has updates on my journey this far. I look forward to hearing from you!
-          If you have any questions please see the Contact page to reach out.
+          I worked in nursing for 12 years and after the intital COVID-19 pandemic began I decided to persue a
+          career in a field I have always wanted to work in. I have obtained a FulL-Stack Development Certification,
+          Mobile Development Certification through Bottega University. I also completed the Google Data Analyst Certification.
+          I look forward to hearing from you! If you have any questions please see the Contact page to reach out.
         </p>
         <p className="p2">Trenton D. Denton</p>
 
